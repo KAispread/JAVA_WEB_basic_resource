f8b4edf3d659c4184917e7a4a3b52db03b920116
KAispread <pain0121@naver.com>
2022-08-27 20:49:45 +0900

    데이터베이스 연동 예제

---
+++ pro07/src/main/java/sec02/ex01/MemberDAO.java
@@ -0,0 +1,68 @@
+package sec02.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class MemberDAO {
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			
+			// context.xml에 설정한 name 값으로 톰캣이 미리 연결한 DataSource를 받아옴
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		
+		try {
+			con = dataFactory.getConnection(); //DataSource를 이용해 데이터베이스에 연결
+			
+			String query = "select * from t_member ";
+			System.out.println(query);
+			
+			pstmt = con.prepareStatement(query); // query를 전달하여 PreparedStatement 객체를 생성.
+			ResultSet rs = pstmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
